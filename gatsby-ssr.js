836560f9c6d319f7b59ac2cab4b43928f848be3c
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2023-06-15 09:50:37 +0200

    added crm details

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,14 +2,19 @@ import Nbcollective from "./src/root-wrapper"
 import React from "react"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 
-// export function onRenderBody({ setHeadComponents }) {
-//   setHeadComponents([
-//     <link
-//       rel="stylesheet"
-//       href="node_modules/react-responsive-carousel/lib/styles/carousel.min.css"
-//     >,
-//   ])
-// }
+export function onRenderBody({ setHeadComponents }) {
+  setHeadComponents([
+    <script
+      type="text/javascript"
+      src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=YsZkRA"
+    ></script>,
+  ])
+  // setPreBodyComponents([
+  //   <div id="checkout">
+  //     <div id="paypal-button-container"></div>
+  //   </div>,
+  // ])
+}
 
 const Wrapper = ({ element }) => {
   return <Nbcollective element={element} />
