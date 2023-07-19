717af8a98ee471a6afaed00e791994286466c59c
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2023-07-19 12:04:40 +0200

    added omnisend

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,6 +8,17 @@ export function onRenderBody({ setHeadComponents }) {
       type="text/javascript"
       src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=YsZkRA"
     ></script>,
+    ,
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+        window.omnisend = window.omnisend || [];
+        omnisend.push(["accountID", "64b7b48bf3fd91671509b42f"]);
+        omnisend.push(["track", "$pageViewed"]);
+        !function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://omnisnippet1.com/inshop/launcher-v2.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
+   	 `,
+      }}
+    />,
   ])
   // setPreBodyComponents([
   //   <div id="checkout">
